16d875ffe96b435398f0f53cef9d62f246de9aa7
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-11 12:31:13 -0500

    add: navbar popup text

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
+// router
+import { useRouter } from "next/router";
+
+// framer motion
+import { AnimatePresence, motion } from "framer-motion";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <Layout>
       <Component {...pageProps} />;
