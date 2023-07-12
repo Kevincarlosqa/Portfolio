dd32e0a6fe87e3a1ad43b3329126295ac48116f3
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-12 12:05:12 -0500

    add: Testimonial Slider

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,30 +1,78 @@
+// import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
+
+// icons
+import { BsArrowRight } from "react-icons/bs";
+
+// next Image
+import Image from "next/image";
+
 // testimonial data
 const testimonialData = [
   {
-    image: '/t-avt-1.png',
-    name: 'Anne Smith',
-    position: 'Customer',
+    image: "/t-avt-1.png",
+    name: "Anne Smith",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-2.png',
-    name: 'Jane Doe',
-    position: 'Customer',
+    image: "/t-avt-2.png",
+    name: "Jane Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-3.png',
-    name: 'Jhon Doe',
-    position: 'Customer',
+    image: "/t-avt-3.png",
+    name: "Jhon Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
 ];
 
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="h-[280px] sm:h-[480px]"
+    >
+      {testimonialData.map((testimonial, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div>
+              {/* avatar, name, position */}
+              <div>
+                <div>
+                  {/* avatar */}
+                  <div>avatar image</div>
+                  {/* name */}
+                  <div>name</div>
+                  {/* position */}
+                  <div>position</div>
+                </div>
+              </div>
+              {/* quote & message */}
+              <div>quote & message</div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default TestimonialSlider;
