22ce3ab4dfbb4400b6ed5d228712c18388b17e8d
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-12 12:50:43 -0500

    add: TestimonialsSlider

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,16 +1,16 @@
 // import swiper react components
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// import required modules
+import { Navigation, Pagination } from "swiper";
+
 // import swiper styles
 import "swiper/css";
-import "swiper/css/free-mode";
+import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-// import required modules
-import { Pagination } from "swiper";
-
 // icons
-import { BsArrowRight } from "react-icons/bs";
+import { FaQuoteLeft } from "react-icons/fa";
 
 // next Image
 import Image from "next/image";
@@ -43,30 +43,61 @@ const testimonialData = [
 const TestimonialSlider = () => {
   return (
     <Swiper
-      spaceBetween={10}
+      navigation={true}
       pagination={{
         clickable: true,
       }}
-      modules={[Pagination]}
-      className="h-[280px] sm:h-[480px]"
+      modules={[Navigation, Pagination]}
+      className="h-[400px]"
     >
       {testimonialData.map((testimonial, index) => {
         return (
           <SwiperSlide key={index}>
-            <div>
+            <div
+              className="flex flex-col items-center md:flex-row gap-x-8 h-full
+              px-16"
+            >
               {/* avatar, name, position */}
-              <div>
-                <div>
+              <div
+                className="w-full max-w-[300px] flex flex-col xl:justify-center
+              items-center relative mx-auto xl:mx-0"
+              >
+                <div className="flex flex-col justify-center text-center">
                   {/* avatar */}
-                  <div>avatar image</div>
+                  <div className="mb-2 mx-auto ">
+                    <Image
+                      src={testimonial.image}
+                      alt=""
+                      width={100}
+                      height={100}
+                    />
+                  </div>
                   {/* name */}
-                  <div>name</div>
+                  <div className="text-lg">{testimonial.name}</div>
                   {/* position */}
-                  <div>position</div>
+                  <div className="text-[12px] uppercase font-extralight tracking-widest">
+                    {testimonial.position}
+                  </div>
                 </div>
               </div>
               {/* quote & message */}
-              <div>quote & message</div>
+              <div
+                className="flex-1 flex flex-col justify-center
+              before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0
+              xl:before:h-[200px] relative xl:pl-20"
+              >
+                {/* quote icon */}
+                <div className="mb-4">
+                  <FaQuoteLeft
+                    className="text-4xl xl:text-6xl text-white/20
+                  mx-auto md:mx-0"
+                  />
+                </div>
+                {/* message */}
+                <div className="xl:text-lg text-center md:text-left">
+                  {testimonial.message}
+                </div>
+              </div>
             </div>
           </SwiperSlide>
         );
