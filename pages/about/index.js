de82b3d4a50337998ee20b3369ba84644dd33398
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-16 20:01:34 -0500

    change: hide counters

--- pages/about/index.js
+++ pages/about/index.js
@@ -149,61 +149,6 @@ const About = () => {
             skills to build complete applications and solutions.
           </motion.p>
           {/* counters */}
-          <motion.div
-            variants={fadeIn("right", 0.6)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8"
-          >
-            <div className="flex flex-1 xl:gap-x-6">
-              {/* experience */}
-              <div
-                className="relative flex-1 after:w-[1px] after:h-full
-              after:bg-white/10 after:absolute after:top-0 after:right-0"
-              >
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={10} duration={5} />+
-                </div>
-                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
-                  Years of experience
-                </div>
-              </div>
-              {/* clients */}
-              <div
-                className="relative flex-1 after:w-[1px] after:h-full
-              after:bg-white/10 after:absolute after:top-0 after:right-0"
-              >
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={250} duration={5} />+
-                </div>
-                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
-                  Satisfied Clients
-                </div>
-              </div>
-              {/* projects */}
-              <div
-                className="relative flex-1 after:w-[1px] after:h-full
-              after:bg-white/10 after:absolute after:top-0 after:right-0"
-              >
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={650} duration={5} />+
-                </div>
-                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
-                  Finished Projects
-                </div>
-              </div>
-              {/* awards */}
-              <div className="relative flex-1">
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <CountUp start={0} end={8} duration={5} />+
-                </div>
-                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
-                  Winning awards
-                </div>
-              </div>
-            </div>
-          </motion.div>
         </div>
         {/* info */}
         <motion.div
@@ -271,3 +216,59 @@ const About = () => {
 };
 
 export default About;
+
+// (<motion.div
+//   variants={fadeIn("right", 0.6)}
+//   initial="hidden"
+//   animate="show"
+//   exit="hidden"
+//   className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8"
+// >
+//   <div className="flex flex-1 xl:gap-x-6">
+//     {/* experience */}
+//     <div
+//       className="relative flex-1 after:w-[1px] after:h-full
+//     after:bg-white/10 after:absolute after:top-0 after:right-0"
+//     >
+//       <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+//         <CountUp start={0} end={10} duration={5} />+
+//       </div>
+//       <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+//         Years of experience
+//       </div>
+//     </div>
+//     {/* clients */}
+//     <div
+//       className="relative flex-1 after:w-[1px] after:h-full
+//     after:bg-white/10 after:absolute after:top-0 after:right-0"
+//     >
+//       <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+//         <CountUp start={0} end={250} duration={5} />+
+//       </div>
+//       <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+//         Satisfied Clients
+//       </div>
+//     </div>
+//     {/* projects */}
+//     <div
+//       className="relative flex-1 after:w-[1px] after:h-full
+//     after:bg-white/10 after:absolute after:top-0 after:right-0"
+//     >
+//       <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+//         <CountUp start={0} end={650} duration={5} />+
+//       </div>
+//       <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+//         Finished Projects
+//       </div>
+//     </div>
+//     {/* awards */}
+//     <div className="relative flex-1">
+//       <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+//         <CountUp start={0} end={8} duration={5} />+
+//       </div>
+//       <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+//         Winning awards
+//       </div>
+//     </div>
+//   </div>
+// </motion.div>)
