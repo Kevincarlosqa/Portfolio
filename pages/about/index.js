3cde450ceabc85171efcf398959c15b92e2480f7
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-16 21:29:06 -0500

    add: change info

--- pages/about/index.js
+++ pages/about/index.js
@@ -53,11 +53,11 @@ const aboutData = [
     title: "Education",
     info: [
       {
-        title: "Webby Awards - Honoree",
+        title: "Web development",
         stage: "2011 - 2012",
       },
       {
-        title: "Adobe Design Achievement Awards - Finalist",
+        title: "Web development",
         stage: "2009 - 2010",
       },
     ],
@@ -66,16 +66,16 @@ const aboutData = [
     title: "experience",
     info: [
       {
-        title: "UX/UI Designer - XYZ Company",
-        stage: "2012 - 2023",
+        title: "Web Developer - ABC Agency",
+        stage: "2023 - 2023",
       },
       {
         title: "Web Developer - ABC Agency",
-        stage: "2010 - 2012",
+        stage: "2023 - 2023",
       },
       {
-        title: "Intern - DEF Corporation",
-        stage: "2008 - 2010",
+        title: "Web Developer - ABC Agency",
+        stage: "2023 - 2023",
       },
     ],
   },
@@ -83,16 +83,16 @@ const aboutData = [
     title: "Hobbies",
     info: [
       {
-        title: "Web Development - ABC University, LA, CA",
-        stage: "2011",
+        title: "Web Developer",
+        stage: "2023",
       },
       {
-        title: "Computer Science Diploma - AV Technical Institute",
-        stage: "2009",
+        title: "Web Developer",
+        stage: "2023",
       },
       {
-        title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
-        stage: "2006",
+        title: "Web Developer",
+        stage: "2023",
       },
     ],
   },
